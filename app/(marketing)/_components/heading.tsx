d93ad241819db45eb0040eca3efe559bd8ef47be
Zaki Ahmad <zakiahmad42661@gmail.com>
2024-02-02 23:41:11 -0500

    created navbar

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -6,7 +6,7 @@ import { ArrowRight } from "lucide-react"
 export const Heading = () =>{
   return (
     <div className="max-w-3xl space-y-4">
-      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
         Your Ideas, Documents, & Plans. Unified. Welcome to 
        {" "}<span className="underline">Zenith</span>
       </h1>
