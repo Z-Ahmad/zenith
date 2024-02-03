224a6d39f1d5b551deb3be08c2faaa021035cb16
Zaki Ahmad <zakiahmad42661@gmail.com>
2024-02-02 23:20:49 -0500

    created landing page and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Zenith",
   description: "Reach Beyond Limits",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.svg",
+        href: "/logo.svg"
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo-dark.svg",
+        href: "/logo-dark.svg"
+      },
+
+    ]
+  }
 };
 
 export default function RootLayout({
