7565c6f119f8f69df2d5fbe6fb8d50be56c39df4
Zaki Ahmad <zakiahmad42661@gmail.com>
2024-02-03 13:36:18 -0500

    added auth functionality, auth check to navbar, and spinner component

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,23 +1,48 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
-import { ArrowRight } from "lucide-react"
+import { useConvexAuth } from "convex/react"
 
-export const Heading = () =>{
+import { ArrowRight } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Spinner } from "@/components/spinner"
+import Link from "next/link"
+import { SignInButton } from "@clerk/clerk-react"
+
+export const Heading = () => {
+  const {isAuthenticated, isLoading} = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
-        Your Ideas, Documents, & Plans. Unified. Welcome to 
-       {" "}<span className="underline">Zenith</span>
+        Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Zenith</span>
       </h1>
       <h3 className="text-base sm:text-xl nd:text-2xl font-medium">
-        Zenith is your essential tool to maximize productivity <br/>
+        Zenith is your essential tool to maximize productivity <br />
         and effortlessly achieve your goals.
       </h3>
-      <Button>
-        Enter Zentith
-        <ArrowRight className="h-4 w-4 ml-2"/>
-      </Button>
+
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Zenith
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Zenith free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
-  )
+  );
 }
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,17 +1,60 @@
 "use client"
+import { useConvexAuth } from "convex/react"
+import { SignInButton, UserButton } from "@clerk/clerk-react"
 
 import { useScrollTop } from "@/hooks/use-scroll-top"
 import { ModeToggle } from "@/components/mode-toggle"
+import { Spinner } from "@/components/spinner"
 import { cn } from "@/lib/utils"
+
+import { Button } from "@/components/ui/button"
 import { Logo } from "./logo"
+import Link from "next/link"
 
 
 export const Navbar = () => {
+  const {isAuthenticated, isLoading} = useConvexAuth();
   const scrolled = useScrollTop()
   return (
     <div className={cn("z-50 bg-background dark:bg-[#111111] fixed top-0 flex items-center w-full p-6", scrolled && "border-b shadow-sm")}>
       <Logo />
       <div className="md:m-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+
+        {isLoading && (
+          <Spinner />
+        )}
+
+        {/* Show login buttons when not logged in */}
+        {!isAuthenticated && !isLoading && (
+          <>
+          <SignInButton mode="modal">
+            <Button variant="ghost" size="sm">
+              Log in
+            </Button>
+          </SignInButton>
+          <SignInButton mode="modal">
+            <Button size="sm">
+              Get Zenith free
+            </Button>
+          </SignInButton>
+          </>
+        )}
+
+        
+        {isAuthenticated && !isLoading && (
+            <>
+              <Button variant="ghost" size={"sm"} asChild>
+                <Link href="/documents">
+                  Enter Zenith
+                </Link>
+              </Button>
+              <UserButton
+                afterSignOutUrl="/"
+              />
+            </>
+          )
+        }
+
         <ModeToggle />
       </div>
     </div>
