f706c8d761d4009366b8287a9c9952dbdd541eb3
Zaki Ahmad <zakiahmad42661@gmail.com>
2024-02-03 17:24:07 -0500

    created document schema and document creation function

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -41,7 +42,8 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
             storageKey="zenith-theme-2"
-            >
+          >
+            <Toaster position="bottom-center"/>
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
