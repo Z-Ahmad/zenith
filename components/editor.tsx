32194462d8ce81e9c5ba758781f3b97d41f4a24b
Zaki Ahmad <zakiahmad42661@gmail.com>
2024-02-09 14:31:06 -0500

    added image upload to editor component

--- components/editor.tsx
+++ components/editor.tsx
@@ -13,6 +13,8 @@ import {
 import "@blocknote/react/style.css"
 import { useTheme } from "next-themes";
 
+import { useEdgeStore } from "@/lib/edgestore";
+
 interface EditorProps {
   onChange: (value: string) => void;
   initialContent?: string;
@@ -22,6 +24,15 @@ interface EditorProps {
 export const Editor = ({onChange, initialContent, editable}: EditorProps) => {
 
   const {resolvedTheme} = useTheme()
+  const {edgestore} = useEdgeStore();
+
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({
+      file
+    })
+
+    return response.url
+  }
 
   const editor: BlockNoteEditor = useBlockNote({
     editable,
@@ -29,6 +40,7 @@ export const Editor = ({onChange, initialContent, editable}: EditorProps) => {
     onEditorContentChange: (editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2))
     },
+    uploadFile: handleUpload
   })
 
   return (
